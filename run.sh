908d96ff8aa7c703681bc1c90787af5de2f16f38
Philipp Haussleiter <philipp@haussleiter.de>
2013-12-07 01:25:43 +0100

    adding vm info

--- run.sh
+++ run.sh
@@ -1,15 +1,34 @@
 #!/usr/bin/env node
 
-var vmrum = require('./lib/vmrun');
+var vmrun = require('./lib/vmrun');
 
-vmrum.info(function(err, data){
+vmrun.info(function(err, data){
+	console.log("\nvmrun.info:\n");
 	console.log(data);
 });
 
-vmrum.listAll(function(err, data){
+vmrun.listAll(function(err, data){
+	console.log("\nvmrun.listAll:\n");
 	console.log(data);
 });
 
-vmrum.listRunning(function(err, data){
+vmrun.showAll(function(err, data){
+	console.log("\nvmrun.showAll:\n");
 	console.log(data);
 });
+
+vmrun.listRunning(function(err, data){
+	console.log("\nvmrun.listRunning:\n");
+	console.log(data);
+});
+
+vmrun.showRunning(function(err, data){
+	console.log("\nvmrun.showRunning:\n");
+	console.log(data);
+});
+
+vmrun.vminfo(0, function(err, data){
+	console.log("\nvmrun.vminfo:\n");
+	console.log(data);
+});
+
